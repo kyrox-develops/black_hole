a0ce4ef74cfb23617f78519fe5b3fd873d7d6f62
sriharansiva10 <sriharansiva242@gmail.com>
2026-06-15 16:45:02 +0530

    window added and glad folders added

--- black_hole.cpp
+++ black_hole.cpp
@@ -1,6 +1,23 @@
-#include<iostream>
-using namespace std;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main() {
-    cout << "hello world";
+    glfwInit();
+
+    GLFWwindow* window =
+        glfwCreateWindow(800, 600, "black hole", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
